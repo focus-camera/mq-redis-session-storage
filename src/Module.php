f6f29deaf3d13fe84f7e830b1bb9ff395d358d83
Michael Grinspan <mgrinspan@focuscamera.com>
2020-06-08 12:15:52 -0400

    Reformatted code and switched to PSR-4

--- src/Module.php
+++ src/Module.php
@@ -40,17 +40,6 @@ class Module {
 	 * @return mixed
 	 */
 	public function getConfig() {
-		return include __DIR__ . '/config/module.config.php';
-	}
-
-	/**
-	 * @return array
-	 */
-	public function getAutoloaderConfig() {
-		return [
-			'Zend\Loader\ClassMapAutoloader' => [
-				__DIR__ . '/autoload_classmap.php',
-			],
-		];
+		return include __DIR__ . '/../config/module.config.php';
 	}
 }
